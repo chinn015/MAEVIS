6f730ddfd3459c41501397d083a5331fc3ce8925
Christine Diane Malazarte <chenmalazarte015@gmail.com>
2017-12-08 14:37:15 +0800

    HomePage and User Profile - frontend enhancements

--- app/src/main/java/com/user/maevis/TabHomeAdapter.java
+++ app/src/main/java/com/user/maevis/TabHomeAdapter.java
@@ -25,7 +25,7 @@ public class TabHomeAdapter extends RecyclerView.Adapter<TabHomeAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_home_items, parent, false);
         return new ViewHolder(v);
     }
 
