339d683487ca1e38311d1440432df7c28751beaf
Christine Diane Malazarte <chenmalazarte015@gmail.com>
2017-12-09 23:58:13 +0800

    Reports on the Home page will be clickable

--- app/src/main/java/com/user/maevis/TabHomeAdapter.java
+++ app/src/main/java/com/user/maevis/TabHomeAdapter.java
@@ -1,12 +1,15 @@
 package com.user.maevis;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -31,12 +34,19 @@ public class TabHomeAdapter extends RecyclerView.Adapter<TabHomeAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        ListItem listItem = listItems.get(position);
+        final ListItem listItem = listItems.get(position);
 
         holder.textViewHead.setText(listItem.getHead());
         holder.textViewDesc.setText(listItem.getDesc());
         holder.textViewDateTime.setText(listItem.getDateTime());
         Picasso.with(context).load(listItem.getImageURL()).into(holder.imageViewReport);
+
+        holder.reportLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(context, "You clicked : " + listItem.getHead(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -50,6 +60,7 @@ public class TabHomeAdapter extends RecyclerView.Adapter<TabHomeAdapter.ViewHold
         public TextView textViewDesc;
         public TextView textViewDateTime;
         public ImageView imageViewReport;
+        public LinearLayout reportLayout;
 
         public ViewHolder (View itemView){
             super(itemView);
@@ -58,6 +69,7 @@ public class TabHomeAdapter extends RecyclerView.Adapter<TabHomeAdapter.ViewHold
             textViewDesc = (TextView) itemView.findViewById(R.id.textViewDesc);
             textViewDateTime = (TextView) itemView.findViewById(R.id.textViewDateTime);
             imageViewReport = (ImageView) itemView.findViewById(R.id.imageViewReport);
+            reportLayout = (LinearLayout) itemView.findViewById(R.id.reportLayout);
         }
     }
 }
