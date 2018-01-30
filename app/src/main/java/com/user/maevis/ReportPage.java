2ac760418d9e02af763a8009b8a205f4b1d9779f
seppjooo <seppjooo@gmail.com>
2018-01-30 03:44:05 -0800

    Home Location Notif

--- app/src/main/java/com/user/maevis/ReportPage.java
+++ app/src/main/java/com/user/maevis/ReportPage.java
@@ -52,30 +52,6 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
         viewReportType = (ImageView) findViewById(R.id.viewReportType);
         viewUserImage = (CircleImageView) findViewById(R.id.imgViewProfilePic);
 
-        /*if(TabHomeAdapter.getClickedItemVerified() != null) {
-            viewReportHead.setText(TabHomeAdapter.getClickedItemVerified().getHead());
-            viewReportDesc.setText(TabHomeAdapter.getClickedItemVerified().getDescription());
-            viewReportDateTime.setText(TabHomeAdapter.getClickedItemVerified().getDisplayDateTime());
-            Picasso.with(getApplicationContext())
-                    .load(TabHomeAdapter.getClickedItemVerified().getImageThumbnailURL())
-                    .fit()
-                    .into(viewReportImage);
-            Picasso.with(getApplicationContext())
-                    .load(ListItem.getReportTypeImage(TabHomeAdapter.getClickedItemVerified().getReportType()))
-                    .into(viewReportType);
-        }else{
-            viewReportHead.setText(Tab2_Location.activeVerifiedReport.getHead());
-            viewReportDesc.setText(Tab2_Location.activeVerifiedReport.getDescription());
-            viewReportDateTime.setText(Tab2_Location.activeVerifiedReport.getDisplayDateTime());
-            Picasso.with(getApplicationContext())
-                    .load(Tab2_Location.activeVerifiedReport.getImageThumbnailURL())
-                    .fit()
-                    .into(viewReportImage);
-            Picasso.with(getApplicationContext())
-                    .load(ListItem.getReportTypeImage(Tab2_Location.activeVerifiedReport.getReportType()))
-                    .into(viewReportType);
-        }*/
-
         if(PageNavigationManager.getClickedTabHomeListItemVerified() != null) {
             viewReportHead.setText(PageNavigationManager.getClickedTabHomeListItemVerified().getHead());
             viewReportDesc.setText(PageNavigationManager.getClickedTabHomeListItemVerified().getDescription());
@@ -131,12 +107,7 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
             if(SessionManager.getUserType().equals("Admin")) {
                 Toast.makeText(this, "User Management", Toast.LENGTH_LONG).show();
 
-                if (PageNavigationManager.getClickedTabHomeListItemVerified() != null /*TabHomeAdapter.clickedStatus*/) {
-                    /*clickedUserID = PageNavigationManager.getClickedTabHomeListItemVerified().getReportedBy();
-                    tab = "TabLoc";
-                    listItemTemp = PageNavigationManager.getClickedTabLocListItemPending();
-                    Toast.makeText(this, "[TabLoc] User ID: "+clickedUserID, Toast.LENGTH_LONG).show();*/
-
+                if (PageNavigationManager.getClickedTabHomeListItemVerified() != null) {
                     PageNavigationManager.markTab(PageNavigationManager.getClickedTabHomeListItemVerified().getReportedBy(),
                             PageNavigationManager.KEY_TABHOME,
                             PageNavigationManager.getClickedTabHomeListItemVerified());
@@ -144,11 +115,7 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
                     Toast.makeText(this, "[TabLoc] User ID: "+PageNavigationManager.getClickedUserID(), Toast.LENGTH_LONG).show();
                 }
 
-                if (PageNavigationManager.getClickedTabNotifListItem() != null /*TabNotifAdapter.clickedStatus*/) {
-                    /*clickedUserID = PageNavigationManager.getClickedTabLocListItemPending().getReportedBy();
-                    tab = "TabLoc";
-                    listItemTemp = PageNavigationManager.getClickedTabLocListItemPending();*/
-
+                if (PageNavigationManager.getClickedTabNotifListItem() != null) {
                     PageNavigationManager.markTab(PageNavigationManager.getClickedTabNotifListItem().getReportedBy(),
                             PageNavigationManager.KEY_TABNOTIF,
                             PageNavigationManager.getClickedTabNotifListItem());
@@ -157,10 +124,6 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
                 }
 
                 if (PageNavigationManager.getClickedTabLocListItemVerified() != null) {
-                    /*clickedUserID = PageNavigationManager.getClickedTabLocListItemPending().getReportedBy();
-                    tab = "TabLoc";
-                    listItemTemp = PageNavigationManager.getClickedTabLocListItemPending();*/
-
                     PageNavigationManager.markTab(PageNavigationManager.getClickedTabLocListItemVerified().getReportedBy(),
                             PageNavigationManager.KEY_TABLOCVERIFIED,
                             PageNavigationManager.getClickedTabLocListItemVerified());
@@ -173,29 +136,9 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
                     Intent i = new Intent(this, UserManagement.class);
                     startActivity(i);
                 }
-
-                /*for (int x = 0; x < FirebaseDatabaseManager.getUserItems().size(); x++) {
-                    if (userID.equals(FirebaseDatabaseManager.getUserItems().get(x).getUserID())) {
-                        clickedUserItem = FirebaseDatabaseManager.getUserItems().get(x);
-                        clickedUserItemStatus = true;
-
-                        PageNavigationManager.clickReportPageUserItem(FirebaseDatabaseManager.getUserItems().get(x));
-
-                        /*VerifyReport.clickedUserItemStatus = false;
-                        TabHomeAdapter.clickedUserItemStatus = false;
-                    }
-                }*/
             }
         }
     }
-
-    /*public static UserItem getClickedUserItem() {
-        return clickedUserItem;
-    }
-
-    public static void setClickedUserItem(UserItem clickedUserItem) {
-        ReportPage.clickedUserItem = clickedUserItem;
-    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -223,7 +166,6 @@ public class ReportPage extends AppCompatActivity  implements View.OnClickListen
             finish();
 
         }
-
 
         TabProfileTimelineAdapter.setClickedItem(null);
         PageNavigationManager.setClickedTabHomeListItemVerified(null);
